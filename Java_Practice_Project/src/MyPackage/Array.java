d4c3d3d05042ab172ddefbc44831c1636c69dfd1
Pratik <pratiquebangal@gmail.com>
2018-04-27 17:18:09 +0530

    Array commit

---
+++ Java_Practice_Project/src/MyPackage/Array.java
@@ -0,0 +1,18 @@
+package MyPackage;
+
+public class Array {
+	
+	public static void main(String[]args) {
+		
+		int [] a; //Declaration
+		
+		a = new int[5]; //Allocation
+		
+		a[0] = 1; //Initialization
+		
+		System.out.println("Value at index 0 is " + a[0]);
+		
+		
+	}
+
+}
