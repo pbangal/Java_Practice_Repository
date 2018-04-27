45057ea899edf916bcfb7eb7a4f52f3480c0d403
Pratik <pratiquebangal@gmail.com>
2018-04-27 17:19:10 +0530

    Commit

---
+++ Java_Practice_Project/src/MyPackage/Addition_Return.java
@@ -0,0 +1,17 @@
+package MyPackage;
+
+public class Addition_Return {
+	
+	public static void main(String[]args) {
+		
+		int a = addition();
+		System.out.println("Addition is " + a);
+	}
+
+	static int addition() 
+	{
+		int c = 20+30;
+		return c;
+		
+	}
+}
