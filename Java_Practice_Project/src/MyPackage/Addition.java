8ccb47ed6cf4dab737bb0df56472bb8fc0daaa18
Pratik <pratiquebangal@gmail.com>
2018-04-27 15:36:01 +0530

    Second commit

---
+++ Java_Practice_Project/src/MyPackage/Addition.java
@@ -0,0 +1,18 @@
+package MyPackage;
+
+public class Addition {
+	
+	public static void main(String[]args) {
+		
+		int a;
+		int b;
+		int c;
+		
+		a=20;
+		b=30;
+		c=a+b;
+		
+		System.out.println("Addition of a and b is " +c);
+	}
+
+}
