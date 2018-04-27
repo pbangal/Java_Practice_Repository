c42c2c3b87ee5a96775feedd442950d3d7dd0005
Pratik <pratiquebangal@gmail.com>
2018-04-27 17:03:31 +0530

    Commit

---
+++ Java_Practice_Project/src/MyPackage/MethodDemo2.java
@@ -0,0 +1,17 @@
+package MyPackage;
+
+public class MethodDemo2 {
+	
+	static void addition(int a, int b)
+	{
+		int c =a+b;
+		System.out.println("Addition of a and b is " +c);
+		
+	}
+	
+	public static void main(String[]args) {
+		
+		addition(50,60);
+	}
+
+}
