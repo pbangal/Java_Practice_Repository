cde46db232a0dc00b05d3273ec78b9d706b5abe0
Pratik <pratiquebangal@gmail.com>
2018-04-27 17:32:10 +0530

    Long Commit

---
+++ Java_Practice_Project/src/MyPackage/LongArray.java
@@ -0,0 +1,21 @@
+package MyPackage;
+
+public class LongArray {
+
+	public static void main(String[]args) {
+		
+		int [] array;
+		
+		array = new int[4];
+		
+		array[0] = 1;
+		array[1] = 3;
+		array[2] = 5;
+		array[3] = 7;
+		
+		System.out.println("Value at index 0 is " + array[0]);
+		System.out.println("Value at index 1 is " + array[1]);
+		System.out.println("Value at index 2 is " + array[2]);
+		System.out.println("Value at index 3 is " + array[3]);
+	}
+}
