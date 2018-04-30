9a1b6f39038c6061965dd8d9acc446e330e8d3ab
Pratik <pratiquebangal@gmail.com>
2018-04-30 12:24:54 +0530

    Initial commit

---
+++ Java_Practice_Project/src/MyPackage/ArrayD1.java
@@ -0,0 +1,18 @@
+package MyPackage;
+
+public class ArrayD1 {
+
+	public static void main(String[]args) {
+		
+		int [] a;
+		a = new int[3];
+		a[0] = 5;
+		a[1] = 10;
+		a[2] = 15;
+		
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
