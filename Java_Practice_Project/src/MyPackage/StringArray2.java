28759e6b275f015a148e78ce071abfd80ab38dac
Pratik <pratiquebangal@gmail.com>
2018-05-02 16:10:34 +0530

    Commit Two

---
+++ Java_Practice_Project/src/MyPackage/StringArray2.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class StringArray2 {
+
+	public static void main(String[]args) {
+		
+		System.out.println("Total arguments are " + args.length);
+		
+		for (int i=0; i< args.length; i++)
+		{
+			System.out.println(args[i]);
+		}
+	}
+}
